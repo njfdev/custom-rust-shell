ffa8a0455d9236890a78419ae2f27b43a94e23cf
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-11 16:19:01 -0400

    Add Command Not Found Logic

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,15 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // remove newline
+    input.pop();
+
+    // get the command name (1st argument)
+    let command_name = input.split(' ').nth(0);
+
+    // If command is not handled, print out a message
+    if command_name.is_some() && command_name.unwrap().len() > 0 {
+        println!("{}: command not found", command_name.unwrap());
+    }
 }
