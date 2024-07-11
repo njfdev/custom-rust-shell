6c99179f810493eda359464df404b23b9a4429cb
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-11 16:21:54 -0400

    Implement REPL (Read-Eval-Print-Loop)

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // remove newline
-    input.pop();
+        // remove newline
+        input.pop();
 
-    // get the command name (1st argument)
-    let command_name = input.split(' ').nth(0);
+        // get the command name (1st argument)
+        let command_name = input.split(' ').nth(0);
 
-    // If command is not handled, print out a message
-    if command_name.is_some() && command_name.unwrap().len() > 0 {
-        println!("{}: command not found", command_name.unwrap());
+        // If command is not handled, print out a message
+        if command_name.is_some() && command_name.unwrap().len() > 0 {
+            println!("{}: command not found", command_name.unwrap());
+        }
     }
 }
